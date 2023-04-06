0da014c8958121951218ae0ef2f6a86bbd011ad2
Mehmet Secgin <mehmetascgn@gmail.com>
2023-04-06 15:09:02 +0300

    Setup and readme added

---
+++ HW1/README.md
@@ -0,0 +1,3 @@
+# square_mehmetsecgin
+
+This is a Python module that provides a simple function to calculate the square of the sum of two numbers.
---
+++ HW1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='square_mehmetsecgin',
+    version='0.1',
+    description='A simple module that returns the square of the sum of two numbers',
+    url='https://github.com/MehmetSecgin/AdvancedPython/tree/main/HW1/square_mehmetsecgin',
+    author='Mehmet Secgin',
+    author_email='mehmetascgn@gmail.com',
+    packages=['square_mehmetsecgin'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
